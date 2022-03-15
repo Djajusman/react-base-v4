dd3c27dec546032281dff880050fbeb94d6a1141
Djajusman <tsaniyadiriyadh@gmail.com>
2022-03-15 19:19:45 +0700

    update docker and fcm

--- mainapp/src/App.jsx
+++ mainapp/src/App.jsx
@@ -5,6 +5,10 @@ import FirebaseMessaging from "./config/initFirebase.js";
 
 import "./index.scss";
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <FirebaseMessaging />

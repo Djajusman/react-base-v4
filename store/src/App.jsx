2b91686348f350cb7faebc4df022374a24827480
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-18 10:41:52 +0700

    add social media and pwa

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -5,10 +5,8 @@ import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: store</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <div>Name: Redux</div>
+
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));

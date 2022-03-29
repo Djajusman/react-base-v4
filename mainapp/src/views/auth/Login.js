be6eb13bbbda83e1e247d26872fa05a35d16c195
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-29 18:31:37 +0700

    add validasi

--- mainapp/src/views/auth/Login.js
+++ mainapp/src/views/auth/Login.js
@@ -42,11 +42,6 @@ export default function Login() {
     if (!values.username) {
       errors.username = "Username is required!";
     }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
     if (!values.password) {
       errors.password = "Password is required";
     } else if (values.password.length < 4) {
